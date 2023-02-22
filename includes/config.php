0b561a0bddeceb49132b3879a546a0a7091da86e
mustbebuilt <martin@mustbebuilt.co.uk>
2023-02-22 16:25:37 +0000

    Environment variables set

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,10 @@
 <?php
 /* Database credentials */
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "");
-define("DB_PASSWORD", "");
-define("DB_NAME", "");
+//DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD
+define("DB_SERVER", $_ENV["DB_HOST"] );
+define("DB_USERNAME", $_ENV["DB_USERNAME"]);
+define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
+define("DB_NAME", $_ENV["DB_DATABASE"]);
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
